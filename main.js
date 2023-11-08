47c6b7b07fc39b96ca8a7b2bb3f782eed2d2b95a
joaovictorlemos <130058940+joaovictorlemos@users.noreply.github.com>
2023-11-08 08:17:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert('fala meus mano do 2d');
+
+function tocaSom (idElementoAudio){
+document.querySelector(idElementoAudio).play();}
+const keyList=document.querySelectorAll('tecla');
+let contador = 0;
+ while(contador <keylist.length)
+const efeito= listadeteclas [contador].classList[1];
+const idAudio="#som_"+efeito;
+listadeteclas [contador].onclick=function(){
+tocaSom(idÁudio);} 
+contador-contador +1 ;
+console.log(contador);
+
+tecla.onkeydown= function(){
+    tecla.add('ativa');
+}
